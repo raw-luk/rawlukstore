6f3c248ff93791dcdb5543cd66993acad0eeaf0a
raw-luk <132921322+raw-luk@users.noreply.github.com>
2023-05-08 20:58:45 +0530

    Update settings.py

--- Rawluk_Project/settings.py
+++ Rawluk_Project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*9dc3*p^jk28xt!cbb&p7*q^3bcioc-#equpedzjw@nymu=a+#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
